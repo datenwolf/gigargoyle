29494d6d2f9b2f9a76b132cd927e2d39a2514398
Raffael Mancini <raffael.mancini@hcl-club.lu>
2010-07-16 17:54:04 +0200

    Added a simple gg client library

---
+++ simpleclient.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "gg_simple_client.h"
+
+#define COLS 24
+#define ROWS 4
+#define FRAME_DURATION 1.0f/30*1e6
+
+int main(int argc, char *argv[]) {
+  gg_frame *f;
+  gg_socket *s;
+
+  int i, col, row;
+
+  f = gg_init_frame(24, 4, 8);
+  s = gg_init_socket("localhost", 0xabac);
+
+  /* 30 fps */
+  gg_set_duration(s, FRAME_DURATION);
+
+  for (i = 0; i < 100; ++i) {
+
+    for (col = 0; col < COLS; ++col) {
+      for (row = 0; row < ROWS; ++row) {
+        gg_set_pixel_color(f, row, col,
+                           i%255, i%255, i%255);
+      }
+    }
+
+    printf("Sent frame %d\n", i);
+    gg_send_frame(s, f);
+
+    /* usleep(FRAME_DURATION); */
+  }
+
+  gg_deinit_frame(f);
+  gg_deinit_socket(s);
+
+  return 0;
+}
