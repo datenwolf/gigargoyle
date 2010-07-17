92f750a671425f83072119c119d216af0cccb428
Raffael <raffael.mancini@hcl-club.lu>
2010-07-17 23:32:11 +0200

    works

--- simpleclient.c
+++ simpleclient.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "gg_simple_client.h"
 
 #define COLS 24
 #define ROWS 4
-#define FRAME_DURATION 1.0f/5*1e6
+#define FRAME_DURATION 1.0f/30*1e6
 
 int main(int argc, char *argv[]) {
   gg_frame *f;
@@ -17,23 +18,21 @@ int main(int argc, char *argv[]) {
   s = gg_init_socket("localhost", 0xabac);
 
   /* 30 fps */
-  /* gg_set_duration(s, FRAME_DURATION); */
+  gg_set_duration(s, FRAME_DURATION);
 
-  for (i = 0; i < 1000; ++i) {
+  for (i = 0; i < 2000; ++i) {
 
     for (col = 0; col < COLS; ++col) {
       for (row = 0; row < ROWS; ++row) {
         gg_set_pixel_color(f, col, row,
-                           row*10, col*4, 0);
+                           (uint8_t)(127*(sin(i/10.0)+1)),
+                           (uint8_t)(127*(sin(i/10.1)+1)),
+                           (uint8_t)(127*(sin(i/10.2)+1)));
       }
     }
 
-    gg_send_command(s, PKT_TYPE_SET_SCREEN_WHT);
-
     printf("Sent frame %d\n", i);
-    /* gg_send_frame(s, f); */
-
-    /* usleep(FRAME_DURATION-100); */
+    gg_send_frame(s, f);
   }
 
   gg_deinit_frame(f);
